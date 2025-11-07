35ca1916be78444a2e4a48a7ade2f557e7444f38
K10-K10 <keito.tottori@gmail.com>
2025-11-07 16:18:30 +0900

    Fix:regex

--- main.cpp
+++ main.cpp
@@ -3,39 +3,58 @@
 #include <iostream>
 #include <regex>
 #include <string>
+std::regex brCheck(R"([^\s]+)");
+std::regex H(R"(^\s*(#{1,6})\s(.*))");
+std::regex Br(R"(\s{2,2}$)");
+std::regex EB(R"([*]{3,3}([^*]+)[*]{3,3})");
+std::regex Bold(R"([*]{2,2}([^*]+)[*]{2,2}|[_]{2,2}([^_]+)[_]{2,2})");
+std::regex Em(R"([*]([^*]+)[*]|[_]([^_]+)[_])");
+std::regex Hr(R"(^([-_*])\1{2,}+$)");
+std::regex S(R"([^~]~([^~]+)~[^~]|~~([^~]+)~~)");
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   std::string fileName, htmlFile = "a.html";
-  if (argc < 2) {
+  if (argc < 2)
+  {
     std::cout << "Error: File can't open" << std::endl;
     return 0;
-  } else {
+  }
+  else
+  {
     fileName = argv[1];
     htmlFile = argc == 2 ? "a.html" : argv[2];
   }
   std::ofstream oFile(htmlFile, std::ios::trunc);
   std::ifstream iFile(fileName);
-  std::string line;
   oFile << "<!DOCTYPE HTML>\n<body>" << std::endl;
-  std::regex Br(R"(\s{2,2}$)");
-  std::regex Bold(R"([_\*][_\*](.*?)[_\*][_\*])");
-  std::regex Em(R"([_\*](.*?)[_\*])");
-  std::regex Hr(R"(^[-]{3,}+$)");
-  std::regex S1(R"(~(.*?)~)"), S2(R"(~~(.*?)~~)");
+
+  bool br_flag = false;
+
+  std::string line;
   std::string latest_line;
-  while (std::getline(iFile, line)) {
-    if (line == "")
-      line = "<br><br>";
-    std::string replaced_br = std::regex_replace(line, Br, "<br>");
-    std::string replaced_hr = std::regex_replace(replaced_br, Hr, "<hr>");
-    std::string replaced_bold =
-        std::regex_replace(replaced_hr, Bold, "<strong>$1</strong>");
-    std::string replaced_em =
-        std::regex_replace(replaced_bold, Em, "<em>$1</em>");
-    latest_line = replaced_em;
-    latest_line = std::regex_replace(latest_line, S2, "<s>$1</s>");
-    latest_line = std::regex_replace(latest_line, S1, "<s>$1</s>");
-    // latest_line = std::regex_replace(latest_line,Sub,)
+  std::smatch m;
+  while (std::getline(iFile, line))
+  {
+    latest_line = line;
+    latest_line = line = "" && br_flag ? "<br>" : "<br><br>";
+    if (!br_flag)
+    {
+      latest_line += std::regex_replace(latest_line, Br, "<br>");
+      br_flag = true;
+    }
+    else
+    {
+      if (std::regex_match(latest_line, m, brCheck))
+        br_flag = false;
+    }
+    if (std::regex_match(latest_line, m, H))
+      latest_line = "<h" + std::to_string(m[1].str().length()) + '>' + m[2].str() + "</h" + std::to_string(m[1].str().length()) + '>';
+    latest_line = std::regex_replace(latest_line, Hr, "<hr>");
+    latest_line = std::regex_replace(latest_line, EB, "<strong><em>$1</em></strong>");
+    latest_line = std::regex_replace(latest_line, Bold, "<strong>$2$4</strong>");
+    latest_line = std::regex_replace(latest_line, Em, "<em>$1</em>");
+    latest_line = std::regex_replace(latest_line, S, "<s>$1</s>");
     oFile << latest_line << std::endl;
   }
   oFile << "</body>\n</HTML>" << std::endl;
